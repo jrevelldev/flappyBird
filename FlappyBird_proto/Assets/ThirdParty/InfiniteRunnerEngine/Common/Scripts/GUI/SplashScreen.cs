e930205c58757e8900e1549e03b2eba0e0f6946b
jrevelldev <jrevell@cifog.cat>
2024-11-17 23:21:27 +0100

    Initial upload

---
+++ FlappyBird_proto/Assets/ThirdParty/InfiniteRunnerEngine/Common/Scripts/GUI/SplashScreen.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace MoreMountains.InfiniteRunnerEngine
+{
+	/// <summary>
+	/// Simple start screen class.
+	/// </summary>
+	public class SplashScreen : MonoBehaviour
+	{
+		/// the name of the first level to load
+		[Tooltip("the name of the first level to load")]
+		public string FirstLevel;
+		/// the duration in seconds after which to load the first level
+		[Tooltip("the duration in seconds after which to load the first level")]
+		public float AutoSkipDelay = 2f;
+
+		protected float _delayAfterClick = 1f;
+
+		/// <summary>
+		/// Initialization
+		/// </summary>
+		protected virtual void Start()
+		{
+			GUIManager.Instance.FaderOn(false, 1f);
+
+			if (AutoSkipDelay > 1f)
+			{
+				_delayAfterClick = AutoSkipDelay;
+				StartCoroutine(LoadFirstLevel());
+			}
+		}
+
+		protected virtual IEnumerator LoadFirstLevel()
+		{
+			yield return new WaitForSeconds(_delayAfterClick);
+			GUIManager.Instance.FaderOn(true, 1f);
+			yield return new WaitForSeconds(1f);
+			SceneManager.LoadScene(FirstLevel);
+		}
+	}
+}
---
+++ FlappyBird_proto/Assets/ThirdParty/InfiniteRunnerEngine/Common/Scripts/Managers/StartingPosition.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreMountains.InfiniteRunnerEngine
+{
+	public class StartingPosition : MonoBehaviour { }
+}
